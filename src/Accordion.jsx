3ef2a9b58fb585949c80de5f44c52b417757f375
tucecifci <tucecifcii@gmail.com>
2024-12-10 10:41:25 +0300

    children prop added

--- src/Accordion.jsx
+++ src/Accordion.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import AccordionItem from './AccordionItem'
+import React, { useState } from "react";
+import AccordionItem from "./AccordionItem";
+import { use } from "react";
 
-function Accordion({data}) {
+function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState(null);
+
   return (
-<div className='accordion'>
-    {data.map((el,i) => (<AccordionItem title={el.title} text={el.text} num={i} key={el.title} />))}
-</div>
-  )
+    <div className="accordion">
+      {data.map((el, i) => (
+        <AccordionItem
+          curOpen={curOpen}
+          onOpen={setCurOpen}
+          title={el.title}
+          text={el.text}
+          num={i}
+          key={el.title}
+        > {el.text}
+        </AccordionItem>
+      ))}
+    </div>
+  );
 }
 
-export default Accordion
+export default Accordion;
--- src/AccordionItem.jsx
+++ src/AccordionItem.jsx
@@ -1,17 +1,17 @@
 import React, { useState } from 'react'
 
-function AccordionItem({num, title, text}) {
-    const [isOpen, setIsOpen] = useState(false);
+function AccordionItem({num, title, curOpen, onOpen, children}) {
+    const isOpen = num === curOpen;
     
     function handleToggle(){
-        setIsOpen((isOpen) => !isOpen);
+     onOpen(isOpen ? null : num);
     }
   return (
     <div className= {`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
         <p className='number'>{num < 9 ? `0${num + 1}` : num + 1} </p>
         <p className='title'>{title} </p>
         <p className='icon'>{isOpen ? "-" : "+"} </p>
-        {isOpen && <div className='content-box'>{text} </div>}
+        {isOpen && <div className='content-box'>{children} </div>}
     </div>
   )
 }
